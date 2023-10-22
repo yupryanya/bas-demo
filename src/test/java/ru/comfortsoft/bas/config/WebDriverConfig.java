518d2d537aecd5db2ddf9e9e140785c6a5ea0318
Yulia Azovtseva <yulia.pryania@gmail.com>
2023-10-22 23:46:23 +0400

    Configurations added

--- src/test/java/ru/comfortsoft/bas/config/WebDriverConfig.java
+++ src/test/java/ru/comfortsoft/bas/config/WebDriverConfig.java
@@ -3,9 +3,9 @@ package ru.comfortsoft.bas.config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties",
-        "system:properties"
+        "classpath:${env}.properties"
 })
+
 public interface WebDriverConfig extends Config {
     @Key("browser")
     String browser();
